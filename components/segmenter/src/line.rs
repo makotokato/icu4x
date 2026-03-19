a1f02628b3e110a76f9077aad887115071973f46
Makoto Kato <m_kato@ga2.so-net.ne.jp>
2026-03-19 14:37:40 +0900

    Update

--- components/segmenter/src/line.rs
+++ components/segmenter/src/line.rs
@@ -913,7 +913,7 @@ impl<Y: LineBreakType> Iterator for LineBreakIterator<'_, '_, Y> {
             let after_zwj = lb8a_after_lb9 || (lb9_left.is_none() && left_prop == ZWJ);
             let is_sot = self.get_current_position().unwrap_or(0) == 0;
             if is_sot {
-                // LB20a hack
+                // LB19a/LB20a hack
                 if left_prop == HY
                     || left_prop == HH
                     || left_prop == QU_PI
